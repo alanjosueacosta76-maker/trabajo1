112723549cad250e6f095aaee476a7969969c0bc
alanjosueacosta76-maker <alanjosueacosta76@gmail.com>
2025-09-28 18:37:21 -0700

    Update trabajo1_arreglos.c#

--- trabajo 1/trabajo1_arreglos.c
+++ trabajo 1/trabajo1_arreglos.c
@@ -1,27 +1,24 @@
-#include <stdio.h>
+using System;
 
-int main() {
-    // valores iniciales del arreglo
-    int arreglo[4] = {5, 10, 15, 20};
+class Program
+{
+    static void Main()
+    {
 
-    // asigna un valor por el índice
+        int[] arreglo = { 5, 10, 15, 20 };
 
-    arreglo[100] = 40; // Comportamiento indefinido en C
+        int variable = arreglo[2]; 
+        Console.WriteLine($"variable = {variable}");
 
-    // guarda un valor en la variable
-    int variable = arreglo[2]; // guarda 15 en la variable
-    printf("variable = %d\n", variable);
-
-    // Inicializar con valores por defecto
-    int arreglo2[3] = {0}; // Crea [0, 0, 0]
-
-    // Leer un valor (bucle)
-    for (int i = 0; i < 3; i++) {
-        printf("Indice %d -> %d\n", i, arreglo2[i]);
+ 
+        int[] arreglo2 = new int[3]; 
+        for (int i = 0; i < arreglo2.Length; i++)
+        {
+            Console.WriteLine($"Indice {i} -> {arreglo2[i]}");
+        }
     }
-
-    return 0;
 }
+
 
 
 
