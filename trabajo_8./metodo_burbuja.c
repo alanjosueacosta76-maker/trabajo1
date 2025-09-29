54e75c6bb8a825d6bcde07fc5289dfd7d22a8071
alanjosueacosta76-maker <alanjosueacosta76@gmail.com>
2025-09-28 18:48:37 -0700

    Update metodo_burbuja.c#

--- trabajo_8./metodo_burbuja.c
+++ trabajo_8./metodo_burbuja.c
@@ -1,39 +1,46 @@
-#include <stdio.h>
+using System;
 
-void burbuja(int arr[], int n) {
-    int i, j, temp;
-
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n - 1 - i; j++) {
-            if (arr[j] > arr[j + 1]) {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+class Program
+{
+    static void Burbuja(int[] arr, int n)
+    {
+        int temp;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n - 1 - i; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
             }
         }
     }
-}
 
-
-void imprimirArreglo(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
+    static void ImprimirArreglo(int[] arr, int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(arr[i] + " ");
+        }
+        Console.WriteLine();
     }
-    printf("\n");
+
+    static void Main()
+    {
+        int[] lista = { 5, 3, 8, 4, 2 };
+        int n = lista.Length;
+
+        Console.Write("Lista desordenada: ");
+        ImprimirArreglo(lista, n);
+
+        Burbuja(lista, n);
+
+        Console.Write("Lista ordenada: ");
+        ImprimirArreglo(lista, n);
+    }
 }
 
 
-int main() {
-    int lista[] = {5, 3, 8, 4, 2};
-    int n = sizeof(lista) / sizeof(lista[0]);
-
-    printf("Lista desordenada:");
-    imprimirArreglo(lista, n);
-
-    burbuja(lista, n);
-
-    printf("Lista ordenada: ");
-    imprimirArreglo(lista, n);
-
-    return 0;
-}
